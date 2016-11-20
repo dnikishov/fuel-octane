0b7587be17b89316e1aa2ca870b6c6a5c95721a4
root <root@nailgun.domain.local>
2016-11-20 23:41:01 +0000

    devdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdevdev

--- deployment/puppet/octane_tasks/files/store_current_host.sh
+++ deployment/puppet/octane_tasks/files/store_current_host.sh
@@ -5,7 +5,7 @@ SCRIPT=`readlink -f $0`
 DIR=`dirname ${SCRIPT}`
 
 
-CINDER_HOST=`${DIR}/fetch_puppet_resource_param.sh cinder_config DEFAULT/host value`
-CINDER_BACKEND=`${DIR}/fetch_puppet_resource_param.sh cinder_config DEFAULT/volume_backend_name value`
+CINDER_HOST=`bash ${DIR}/fetch_puppet_resource_param.sh cinder_config DEFAULT/host value`
+CINDER_BACKEND=`bash ${DIR}/fetch_puppet_resource_param.sh cinder_config DEFAULT/volume_backend_name value`
 
 echo "export CURRENT_HOST=\"${CINDER_HOST}#${CINDER_BACKEND}\"" > ${STORE_PATH}
--- deployment/puppet/octane_tasks/files/store_new_host.sh
+++ deployment/puppet/octane_tasks/files/store_new_host.sh
@@ -5,12 +5,12 @@ SCRIPT=`readlink -f $0`
 DIR=`dirname ${SCRIPT}`
 
 
-CINDER_HOST=`${DIR}/fetch_puppet_resource_param.sh cinder_config DEFAULT/host value`
+CINDER_HOST=`bash ${DIR}/fetch_puppet_resource_param.sh cinder_config DEFAULT/host value`
 
 if [[ -z ${CINDER_HOST} ]]; then
-  CINDER_HOST=`${DIR}/fetch_puppet_resource_param.sh cinder_config RBD-backend/backend_host value`
+  CINDER_HOST=`bash ${DIR}/fetch_puppet_resource_param.sh cinder_config RBD-backend/backend_host value`
 fi
 
-CINDER_BACKEND=`${DIR}/fetch_puppet_resource_param.sh cinder_config RBD-backend/volume_backend_name value`
+CINDER_BACKEND=`bash ${DIR}/fetch_puppet_resource_param.sh cinder_config RBD-backend/volume_backend_name value`
 
 echo "export NEW_HOST=\"${CINDER_HOST}#${CINDER_BACKEND}\"" > ${STORE_PATH}
