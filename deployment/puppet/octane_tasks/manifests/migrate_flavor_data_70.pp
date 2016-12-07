b165e7bb4e36876ed41ba534cad0f7a37de019a4
root <root@nailgun.domain.tld>
2016-12-07 16:49:04 +0000

    Fixed conditions to support 6.x

--- deployment/puppet/octane_tasks/manifests/migrate_flavor_data_70.pp
+++ deployment/puppet/octane_tasks/manifests/migrate_flavor_data_70.pp
@@ -5,12 +5,10 @@
 class octane_tasks::migrate_flavor_data_70 (
 ) inherits octane_tasks::params {
 
-  if $octane_tasks::params::fuel_version == '7.0' {
-    exec { 'nova-manage db migrate_flavor_data':
-      command   => 'nova-manage db migrate_flavor_data | grep -q \'0 instances matched query, 0 completed\'',
-      path      => ['/usr/bin', '/usr/sbin', '/bin'],
-      tries     => 10,
-      try_sleep => 10,
-    }
+  exec { 'nova-manage db migrate_flavor_data':
+    command   => 'nova-manage db migrate_flavor_data | grep -q \'0 instances matched query, 0 completed\'',
+    path      => ['/usr/bin', '/usr/sbin', '/bin'],
+    tries     => 10,
+    try_sleep => 10,
   }
 }
